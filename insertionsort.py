7f4177971357317b9e464d8322d095a50f72d6cc
Andrew Gordienko <60705784+AndrewGordienko@users.noreply.github.com>
2020-06-23 14:30:44 -0400

    Add files via upload

---
+++ insertionsort.py
@@ -0,0 +1,18 @@
+ourlist = [2, 4, 3, 1, 5]
+
+def ordered(ourlist):
+    count = 0
+    for i in range(1, len(ourlist)):
+        if ourlist[i] > ourlist[i-1]:
+            count += 1
+    return(count)
+    
+
+while ordered(ourlist) != (len(ourlist) - 1):
+    for i in range(1, len(ourlist)):
+        while ourlist[i] < ourlist[i-1]:
+            a, b = ourlist.index(ourlist[i]), ourlist.index(ourlist[i - 1])
+            ourlist[b], ourlist[a] = ourlist[a], ourlist[b]
+
+print (ourlist)
+        
